f06c443be1e59b27a7d28b8528a87b265f23c137
Robert Ellenberg <rwe24@drexel.edu>
2013-07-22 12:09:00 -0400

    Added missing files

--- mergeSTLFiles.m
+++ mergeSTLFiles.m
@@ -1,5 +1,9 @@
-function mergeSTLFiles()
-files=dir('convhull*.stl');
+function mergeSTLFiles(mask)
+if ~exist('mask','var')
+    mask='convhull*.stl'
+end
+    
+files=dir(mask);
 for f=files'
     filename=f.name();
     disp(filename)
@@ -10,13 +14,14 @@ end
 
 
 function mergeParts(rootname)
-files=dir(['export/',rootname,'*']);
+name=regexprep(rootname,'Body','convhull');
+files=dir(['export/',name,'*']);
 
-if length(files)==0
+if isempty(files)
     disp('No parts found...')
 elseif length(files)==1
     [P,K]=stlread(['export/',files(1).name],true,true);
-    cloud2stl([rootname,'_merged.stl'],P,K);
+    cloud2stl([name,'_merged.stl'],P,K);
 else
     [P,K]=stlread(['export/',files(1).name],true,true);
     for f=files(2:end)'
@@ -24,6 +29,6 @@ else
         [P,K]=mergeFacetSolids(P,K,P2,K2);
     end
     
-    cloud2stl([rootname,'_merged.stl'],P,K);
+    cloud2stl([name,'_merged.stl'],P,K);
 end
 end
